967532ae3dc66a389349308cf5ef95003fd16511
gpt400 <144182051+gpt400@users.noreply.github.com>
2025-03-03 23:40:10 +0000

    Entity functionality added

--- app.py
+++ app.py
@@ -7,31 +7,40 @@ app = Flask(__name__)
 def index():
     if request.method == 'POST':
         hsml_type = request.form.get('hsml_type')
-        # If a type other than Person is selected, alert the user.
-        if hsml_type != 'Person':
+        if hsml_type not in ['Person', 'Entity']:
+            # If an unsupported type is submitted, alert the user.
             return '''
             <script>
                 alert("This functionality is currently being developed. Check back later!");
                 window.location.href = "/";
             </script>
             '''
-        # Process the Person form submission.
-        name = request.form.get('name')
-        birth_date = request.form.get('birth_date')
-        email = request.form.get('email')
-        
-        # Build the HSML JSON object. Only include fields with values.
-        hsml_obj = {
-            "@context": "https://digital-twin-interoperability.github.io/hsml-schema-context/hsml.jsonld",
-            "@type": "Person"
-        }
-        if name:
-            hsml_obj["name"] = name
-        if birth_date:
-            hsml_obj["birthDate"] = birth_date
-        if email:
-            hsml_obj["email"] = email
-
+        if hsml_type == 'Person':
+            name = request.form.get('name')
+            birth_date = request.form.get('birth_date')
+            email = request.form.get('email')
+            hsml_obj = {
+                "@context": "https://digital-twin-interoperability.github.io/hsml-schema-context/hsml.jsonld",
+                "@type": "Person"
+            }
+            if name:
+                hsml_obj["name"] = name
+            if birth_date:
+                hsml_obj["birthDate"] = birth_date
+            if email:
+                hsml_obj["email"] = email
+        elif hsml_type == 'Entity':
+            name = request.form.get('entity_name')
+            description = request.form.get('description')
+            hsml_obj = {
+                "@context": "https://digital-twin-interoperability.github.io/hsml-schema-context/hsml.jsonld",
+                "@type": "Entity"
+            }
+            if name:
+                hsml_obj["name"] = name
+            if description:
+                hsml_obj["description"] = description
+            # swid is intentionally left out.
         json_str = json.dumps(hsml_obj, indent=2)
         return render_template("result.html", json_str=json_str)
     return render_template("index.html")
